a93296ce29fe3e6e81fd556bc61cb287b52abcff
GomezTomas <tomasg@euler.tools>
2022-11-15 18:04:26 -0300

    add alumno

---
+++ src/Alumno.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+
+public class Alumno implements Comparable{
+    private String nombre;
+    private String apellido;
+    private long dni;
+    private int edad;
+    private ArrayList<String> intereses;
+
+    public Alumno(String nombre, String apellido, long dni, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.edad = edad;
+        intereses = new ArrayList<>();
+    }
+
+    public long getDni() {
+        return dni;
+    }
+
+    public ArrayList<String> getIntereses() {
+        return new ArrayList<>(intereses);
+    }
+
+    public void addIntereses(String palabra){
+        if(!intereses.contains(palabra)){
+            intereses.add(palabra);
+        }
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Alumno alumno = (Alumno) o;
+        if (this.apellido.compareTo(alumno.getApellido()) != 0){
+            return this.apellido.compareTo(alumno.getApellido());
+        }
+        if (this.nombre.compareTo(alumno.getNombre()) != 0){
+            return this.nombre.compareTo(alumno.getNombre());
+        }
+
+        return (int) (this.dni - alumno.dni);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -12,36 +12,46 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+//
+//        OrderedList lista = new OrderedList();
+//
+//        lista.insertNode(new Node(3));
+//        lista.insertNode(new Node(3));
+//        lista.insertNode(new Node(1));
+//        lista.insertNode(new Node(1));
+//        lista.insertNode(new Node(1));
+//        lista.insertNode(new Node(1));
+//        lista.insertNode(new Node(2));
+//        lista.insertNode(new Node(4));
+//
+////        lista.deleteNode(new Node(1));
+//
+//        lista.print();
+//
+//        System.out.println("------------------------------------------------");
+//        lista.setOrder(new OrderDesc());
+//        lista.print();
+//
+//
+//        System.out.println("------------------------------------------------");
+//
+//        lista.setOrder(new OrderAsc());
+//        lista.print();
+//        System.out.println("------------------------------------------------");
+//        lista.setOrder(new OrderAsc());
+//        lista.print();
 
-        OrderedList lista = new OrderedList();
+        Alumno a1 = new Alumno("Juan", "Juarez", 33222444, 18);
+        Alumno a2 = new Alumno("Julio", "Cesar", 33222111, 18);
 
-        lista.insertNode(new Node(3));
-        lista.insertNode(new Node(3));
-        lista.insertNode(new Node(1));
-        lista.insertNode(new Node(1));
-        lista.insertNode(new Node(1));
-        lista.insertNode(new Node(1));
-        lista.insertNode(new Node(2));
-        lista.insertNode(new Node(4));
-
-//        lista.deleteNode(new Node(1));
-
-        lista.print();
-
-        System.out.println("------------------------------------------------");
-        lista.setOrder(new OrderDesc());
-        lista.print();
-
-
-        System.out.println("------------------------------------------------");
-
-        lista.setOrder(new OrderAsc());
-        lista.print();
-        System.out.println("------------------------------------------------");
-        lista.setOrder(new OrderAsc());
-        lista.print();
-
+        if (a1.compareTo(a2) < 0){
+            System.out.println("Es menor");
+        }else if (a1.compareTo(a2) > 0){
+            System.out.println("Es mayor");
+        }else{
+            System.out.println("Son iguales");
+        }
 
     }
 }
